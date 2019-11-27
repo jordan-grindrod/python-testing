ee0c111b2a0ccee127e16803d8ea956b71717b1f
Jay Grindrod <36378440+jordan-grindrod@users.noreply.github.com>
2019-11-27 08:52:52 +0000

    Update count_test.py

--- tests/count_test.py
+++ tests/count_test.py
@@ -12,3 +12,6 @@ def test_count_minus():
 
 def test_count_normalNum():
 	assert count.count([1,2,3,4,5,6,6,5,4,3,2,1], 1)==2
+
+def test_count_bigNum():
+	assert count.count([100000,200000,368574,468790,567582,60000,612345,54536,46667,377789,2789,109], 368574)==1
